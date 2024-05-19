7f623d0dbdd80845b48031adaee2880eabe3d282
Tashmit <103169920+TASHMITVERMA@users.noreply.github.com>
2024-05-19 21:26:21 +0530

    Update 222. Count Complete Tree Nodes.cpp

--- 222. Count Complete Tree Nodes.cpp
+++ 222. Count Complete Tree Nodes.cpp
@@ -1,45 +1,53 @@
 class Solution {
 public:
-int f(TreeNode* root)
-{
-    if(root==NULL)
-    return 0;
-    return 1+f(root->left)+f(root->right);
-}
+    int calcLeft(TreeNode* root) {
+        int height = 1;
+        while(root) {
+            height++;
+            root = root->left;
+        }
+        return height;
+    }
+    int calcRight(TreeNode* root) {
+        int height = 1;
+
+        while(root) {
+            height++;
+            root = root->right;
+        }
+        return height;
+    }
+    int calcNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        int leftHeight = calcLeft(root->left);
+        int rightHeight = calcRight(root->right);
+
+        if(leftHeight == rightHeight) {
+            return pow(2,leftHeight)-1;
+        }
+        return 1 + calcNodes(root->left) + calcNodes(root->right);
+    }
     int countNodes(TreeNode* root) {
-        return f(root);
+        return calcNodes(root);
     }
 };
-//tc-O(N)//visit every node once
-//sc-O(N)height of tree times
-//recursive dfs
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 // class Solution {
 // public:
-//     int inorder(TreeNode* root)
-//     {
-//         if (root == NULL)
-//             return 0;
-//         return 1 + inorder(root->left) + inorder(root->right);
-//     }
-
+// int f(TreeNode* root)
+// {
+//     if(root==NULL)
+//     return 0;
+//     return 1+f(root->left)+f(root->right);
+// }
 //     int countNodes(TreeNode* root) {
-//         return inorder(root);
+//         return f(root);
 //     }
 // };
+//tc-O(N)//visit every node once
+//sc-O(N)height of tree times
+//recursive dfs
